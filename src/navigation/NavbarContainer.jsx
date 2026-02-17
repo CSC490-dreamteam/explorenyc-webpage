74927fab3867b6b10f9c2bf58e6915781b933530
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2026-02-17 15:34:06 -0500

    Made button and jsx for settings screen

--- src/navigation/NavbarContainer.jsx
+++ src/navigation/NavbarContainer.jsx
@@ -1,8 +1,9 @@
 import './NavbarContainer.css'
 import HomeScreen from './screens/HomeScreen'
 import NewTripScreen from './screens/NewTripScreen.jsx'
-import SettingScreen from './screens/HistoryScreen.jsx'
+import SettingScreen from './screens/SettingsScreen.jsx'
 import {useState} from "react";
+import HistoryScreen from "./screens/HistoryScreen.jsx";
 
 function NavbarContainer() {
     const [currentScreen, setCurrentScreen] = useState('HomeState')
@@ -11,6 +12,7 @@ function NavbarContainer() {
         switch (currentScreen) {
             case 'HomeState': return <HomeScreen />
             case 'MapState': return <NewTripScreen />
+            case 'HistoryState': return <HistoryScreen />
             case 'SettingsState': return <SettingScreen />
             default: return <HomeScreen />
         }
@@ -19,7 +21,11 @@ function NavbarContainer() {
     return (
         <div className="navbarContainer">
             <div className="navbarContainer__top">
-                Welcome back, User!
+                <span className="navbarContainer__greeting">Welcome back, User!</span>
+                <button className="navbarContainer__settingsButton" onClick={()=>setCurrentScreen('SettingsState')}>
+                    <div className="icon settingsIcon"/>
+                </button>
+
             </div>
 
             <main className="navbarContainer__main">
@@ -35,7 +41,7 @@ function NavbarContainer() {
                     <div className="icon newIcon"/>
                     New Trip
                 </button>
-                <button className="navbarContainer__settingsbutton" onClick={()=>setCurrentScreen('SettingsState')}>
+                <button className="navbarContainer__settingsbutton" onClick={()=>setCurrentScreen('HistoryState')}>
                     <div className="icon mapIcon"/>
                     History
                 </button>
---
+++ src/navigation/screens/SettingsScreen.jsx
@@ -0,0 +1,13 @@
+import '../../App.css'
+import './SettingsScreen.css'
+
+function App() {
+    return (
+        <>
+            <h2> Settings </h2>
+        </>
+    )
+}
+
+
+export default App
