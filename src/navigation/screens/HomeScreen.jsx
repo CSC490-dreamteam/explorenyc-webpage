4b147756c81d70bcf5c4ff66ebfc8d0b5e20fca1
nicholaspapazis <nicholasepapazis@gmail.com>
2026-02-20 12:03:50 -0500

    Implement dynamic recommendation cards and integrate /discover API

--- src/navigation/screens/HomeScreen.jsx
+++ src/navigation/screens/HomeScreen.jsx
@@ -1,8 +1,43 @@
 import '../../App.css'
 import './HomeScreen.css'
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function HomeScreen() {
+
+    //create a "area" to store the places
+    const [places, setPlaces] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    //The API call
+    useEffect(() => {
+        async function fetchDiscover() {
+            try {
+                const res = await fetch(
+                    "http://127.0.0.1:8000/discover"
+                );
+
+                if (!res.ok) {
+                    throw new Error("Failed to fetch /discover");
+                }
+
+                const json = await res.json();
+                setPlaces(json.data); //the API returns {count, data: []} therefore we want the 'data' array
+
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchDiscover();
+    }, []);
+
+    if (loading) return <div>Loading recommendations...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+
+
     return (
         <div className="welcome-container">
             <header className="welcome-header">
@@ -14,12 +49,41 @@ function HomeScreen() {
                 </button>
 
             </header>
-            <div className="for-you">
+            
+
+
+            {/* <div className="for-you">
                 <h3 align='left'>Recommended for you</h3>
                 <div className="card">Place info</div>
                 <div className="card">Place info</div>
                 <div className="card">Place info</div>
+            </div> */}
+            
+            <div className="for-you">
+                <h3 align="left">Recommended for you</h3>
+
+                {places.map((place, idx) => (
+                    <div key={idx} className="card">
+                    <h4>{place.DBA}</h4>
+
+                    <p><strong>Cuisine:</strong> {place["CUISINE DESCRIPTION"]}</p>
+                    <p><strong>Borough:</strong> {place.BORO}</p>
+
+                    <p>
+                        <strong>Address:</strong>{" "}
+                        {place.BUILDING} {place.STREET}, {place.ZIPCODE}
+                    </p>
+
+                    <p><strong>Grade:</strong> {place.GRADE}</p>
+                    </div>
+                ))}
             </div>
+
+
+
+
+
+
         </div>
     )
 }
