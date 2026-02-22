1d173b10172dc23b382ea2686aa036408426d6a7
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2026-02-21 19:27:04 -0500

    Trending card added

--- src/navigation/screens/HomeScreen.jsx
+++ src/navigation/screens/HomeScreen.jsx
@@ -1,8 +1,15 @@
 import '../../App.css'
 import './HomeScreen.css'
+import './components/TrendingCard.jsx'
 import React from "react";
+import TrendingCard from "./components/TrendingCard.jsx";
 
 function HomeScreen() {
+    const trendingSpots = [
+        {id: 1, name: "New York",img:"/public/new-york-city.jpeg"},
+        {id: 2, name: "New York",img:"/public/new-york-city.jpeg"},
+        {id: 3, name: "New York",img:"/public/new-york-city.jpeg"},
+    ]
     return (
         <div className="welcome-container">
             <header className="welcome-header">
@@ -14,8 +21,18 @@ function HomeScreen() {
                 </button>
 
             </header>
+            <div className="trending">
+                <h3 align='left'>🔥 Trending spots</h3>
+                <div className="trending_container">
+
+                    {trendingSpots.map((spot) => (<TrendingCard image={spot.img} key={spot.id} title={spot.name} />))}
+
+                </div>
+
+            </div>
             <div className="for-you">
                 <h3 align='left'>Recommended for you</h3>
+
                 <div className="forYouCard">Place info</div>
                 <div className="forYouCard">Place info</div>
                 <div className="forYouCard">Place info</div>
---
+++ src/navigation/screens/components/TrendingCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './TrendingCard.css'
+
+const TrendingCard = ({ image, title }) => {
+    return (
+        <div className="trending_card">
+            <div className="trending_card_image_container">
+                <img src={image}/>
+            </div>
+            <div style={{paddingTop:'5px'}}>
+                <label style={{padding: '10px'}}> {title} </label>
+                <br/>
+                <label style={{padding: '10px'}}>
+                    ⭐️ 4.6
+                </label>
+                <br/>
+                <label style={{padding: '10px'}}>
+                    🔥+125% visits
+                </label>
+                <label style={{padding: '2px'}}>
+                    📈2.5k Today
+                </label>
+            </div>
+        </div>
+    )
+}
+export default TrendingCard;
