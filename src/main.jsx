95cb702fa9c7511a6303494d2ad5bdf2b0d28a76
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2026-03-05 17:45:45 -0500

    actually added the map

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import NavbarContainer from './navigation/NavbarContainer'
+import MapScreen from "./navigation/screens/MapScreen.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavbarContainer />
+    <MapScreen />
   </StrictMode>,
 )
---
+++ src/navigation/screens/MapScreen.jsx
@@ -0,0 +1,34 @@
+import '../../App.css'
+import './MapScreen.css'
+import React from "react"
+import 'mapbox-gl/dist/mapbox-gl.css'
+import Map from 'react-map-gl/mapbox';
+import mapStyleJson from '../../assets/dark_manhattan.json';
+
+function MapScreen() {
+    const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+
+    const MANHATTAN_BOUNDS = [
+        [-74.047285, 40.679548], // Southwest coordinates
+        [-73.907000, 40.882214]  // Northeast coordinates
+    ];
+
+    return (
+        <div style={{width:'100vw', height:'100vh'}}>
+            <Map
+                mapboxAccessToken={MAPBOX_TOKEN}
+                initialViewState={{
+                    longitude: -73.985,
+                    latitude: 40.748,
+                    zoom: 12.5,
+                }}
+                maxBounds = {MANHATTAN_BOUNDS}
+                minZoom = {1}
+                style={{width:'100%', height:'100%'}}
+                mapStyle={mapStyleJson}
+            />
+        </div>
+    )
+}
+
+export default MapScreen
