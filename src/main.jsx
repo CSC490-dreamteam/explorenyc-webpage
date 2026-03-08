c85034cd9553ef786ba817e5fdb1b3dd31bf2ef2
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2026-03-07 19:26:17 -0500

    reset back to navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import NavbarContainer from './navigation/NavbarContainer'
-import MapScreen from "./navigation/screens/MapScreen.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MapScreen />
+    <NavbarContainer />
   </StrictMode>,
 )
