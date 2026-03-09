472442fcd312a8c2989bf133c01d3ee50a5fea99
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2026-03-09 19:19:37 -0400

    quick disabling. DONT merge this

--- src/navigation/screens/SettingsScreen.jsx
+++ src/navigation/screens/SettingsScreen.jsx
@@ -7,17 +7,16 @@ function App() {
             <h2> Settings </h2>
             <div className="settingsCardsContainer">
                 <div className="settingsCard profileCard">
-                    <div className="icon userpicIcon"/>
-                    <div>
-                        <h2 className="profileCardName">John Doe</h2>
-                        <label>john.doe@email.com</label>
+
+                    <div style={{padding:'8px'}}>
+                        <h2 className="profileCardName">Testing User</h2>
+                        <label>testing@user.com</label>
                     </div>
                 </div>
 
                 <div className="settingsCard categoriesCard">
                     <label className="categoryHeader"><b>Account</b></label>
                     <div className="settingsOption">
-                        <div className="icon userpicIcon"/>
                         Edit Profile
                     </div>
                     <div className="dividerLine"/>
